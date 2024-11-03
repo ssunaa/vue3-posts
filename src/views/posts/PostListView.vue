5bd2fa02f54d7aac1cd83e74855ce5077729c697
shkim <seonflower91@gmail.com>
2024-11-03 16:17:48 +0900

    fix: 첫번째 게시물로 디폴트 셋팅

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -1,3 +1,71 @@
+
+<script setup>
+import PostItem from '@/components/posts/PostItem.vue'
+import PostDetailView from '@/views/posts/PostDetailView.vue'
+import PostFilter from '@/components/posts/PostFilter.vue'
+import PostModal from '@/components/posts/PostModal.vue'
+import { getPosts } from '@/api/posts'
+import { computed, ref, watchEffect, watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+const previewId = ref(null);
+const selectPreview = (id) => (previewId.value = id);
+
+const router = useRouter();
+const posts = ref([]);
+const error = ref(null);
+const loading = ref(false);
+const params = ref({
+  _sort: 'createdAt',
+  _order: 'desc',
+  _page: 1,
+  _limit: 3,
+  title_like: ''
+});
+// pagination
+const totalCount = ref(0);
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit))
+const fetchPosts = async () => {
+  try {
+    loading.value = true;
+    const { data, headers } = await getPosts(params.value);
+    posts.value = data;
+    totalCount.value = headers['x-total-count'];
+  } catch (error) {
+    error.value = error;
+  } finally {
+    loading.value = false;
+  }
+}
+watchEffect(fetchPosts)
+// fetchPosts();
+const goPage = (id) => {
+  // router.push(`/posts/${id}`);
+  router.push({
+    name: 'PostDetail',
+    params: {
+      id
+    }
+  })
+}
+// modal
+const show = ref(false);
+const modalTitle = ref('');
+const modalContent = ref('');
+const modalCreatedAt = ref('');
+const openModal = ({ title, content, createdAt }) => {
+  show.value = true;
+  modalTitle.value = title;
+  modalContent.value = content;
+  modalCreatedAt.value = createdAt;
+}
+
+watch(posts, (target) => {
+  console.log(target[0].id);
+  previewId.value = target[0].id;
+});
+
+</script>
 <template>
   <div>
     <h2>게시글 목록</h2>
@@ -45,68 +113,8 @@
       </AppCard>
     </template>
   </div>
+
 </template>
 
-<script setup>
-import PostItem from '@/components/posts/PostItem.vue'
-import PostDetailView from '@/views/posts/PostDetailView.vue'
-import PostFilter from '@/components/posts/PostFilter.vue'
-import PostModal from '@/components/posts/PostModal.vue'
-import { getPosts } from '@/api/posts'
-import { computed, onMounted, ref, watchEffect } from 'vue'
-import { useRouter } from 'vue-router'
-
-const previewId = ref(null)
-const selectPreview = (id) => (previewId.value = id)
-
-const router = useRouter()
-const posts = ref([])
-const error = ref(null)
-const loading = ref(false)
-const params = ref({
-  _sort: 'createdAt',
-  _order: 'desc',
-  _page: 1,
-  _limit: 3,
-  title_like: ''
-})
-// pagination
-const totalCount = ref(0)
-const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit))
-const fetchPosts = async () => {
-  try {
-    loading.value = true
-    const { data, headers } = await getPosts(params.value)
-    posts.value = data
-    totalCount.value = headers['x-total-count']
-  } catch (error) {
-    error.value = error
-  } finally {
-    loading.value = false
-  }
-}
-watchEffect(fetchPosts)
-// fetchPosts();
-const goPage = (id) => {
-  // router.push(`/posts/${id}`);
-  router.push({
-    name: 'PostDetail',
-    params: {
-      id
-    }
-  })
-}
-// modal
-const show = ref(false)
-const modalTitle = ref('')
-const modalContent = ref('')
-const modalCreatedAt = ref('')
-const openModal = ({ title, content, createdAt }) => {
-  show.value = true
-  modalTitle.value = title
-  modalContent.value = content
-  modalCreatedAt.value = createdAt
-}
-</script>
 
 <style lang="scss" scoped></style>
