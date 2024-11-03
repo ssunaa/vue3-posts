51eb4f628182560620357a0c7b5ef2cfde2b5678
shkim <seonflower91@gmail.com>
2024-11-03 15:26:04 +0900

    add: pinia 기초사용법

--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from '@/router';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from '@/router'
 import globalDirectives from './plugins/global-directive.js'
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.js'
 import funcPlugins from '@/plugins/func.js'
 import objPlugins from '@/plugins/obj.js'
 import personPlugins from '@/plugins/person.js'
 import dayjs from './plugins/dayjs.js'
 // import globalComponents from '@/plugins/global-components';
 
-const app = createApp(App);
+const app = createApp(App)
 // app.use(globalComponents);
 app.use(funcPlugins)
 app.use(objPlugins)
 app.use(personPlugins)
 app.use(globalDirectives)
 app.use(dayjs)
-app.use(router);
-app.mount('#app');
-
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
 
 // console.log('MODE: ', import.meta.env.MODE);
 // console.log('BASE_URL: ', import.meta.env.BASE_URL);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,17 +2,29 @@
   <div>
     <h2>About View</h2>
     <p>{{ $route.path }}</p>
-    <button class="btn btn-primary" @click="$router.push('/')">
-      Home으로 이동
-    </button>
+    <button class="btn btn-primary" @click="$router.push('/')">Home으로 이동</button>
+
+    <p>counter INIT : {{ counter2 }}</p>
+    <p>counter : {{ counter }}</p>
+    <p>doubleCount : {{ doubleCount }}</p>
+    <p>doubleCount : {{ doubleCountPlusOne }}</p>
+    <button @click="increment()">Click!!</button>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia'
 
-const route = useRoute();
-console.log('route.path: ', route.path);
+const route = useRoute()
+console.log('route.path: ', route.path)
+
+const store = useCounterStore()
+const { counter: counter2 } = store;
+const { counter, doubleCount, doubleCountPlusOne } = storeToRefs(store)
+const { increment } = store
+
 </script>
 
 <style lang="scss" scoped></style>
